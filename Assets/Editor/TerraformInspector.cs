95a7e2b25b7dad78877b4ec2c0052b292e56a33c
Roger Larsen <rlarsen@hmstn.com>
2014-04-02 14:30:57 -0500

    made terrain component an inspector, can now change mat props runtime

---
+++ Assets/Editor/TerraformInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Terraform))]
+public class TerraformInspector : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		if(GUILayout.Button("Regenerate")) {
+			var t = (Terraform) target;
+			t.BuildTerrain();
+		}
+	}	
+}
--- Assets/Scripts/Terraform.cs
+++ Assets/Scripts/Terraform.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
+[ExecuteInEditMode]
 public class Terraform : MonoBehaviour {
 
 	public enum TerraformSize {
@@ -10,19 +14,23 @@ public class Terraform : MonoBehaviour {
 	}
 
 	public TerraformSize Size;
-	public Vector3 Position;
-	public Vector3 SmallSize;
-	public Vector3 MediumSize;
-	public Vector3 LargeSize;
+	public Vector3 Position  = new Vector3(0, -0.8f, 0);
+	public Vector3 SmallSize = new Vector3(50, 0.5f, 50);
+	public Vector3 MediumSize = new Vector3(100, 0.5f, 100);
+	public Vector3 LargeSize = new Vector3(150, 0.5f, 150);
 
-	public Material GroundMaterial;
+	//public Material GroundMaterial;
 
-	GameObject ground;
-	GameObject pointLight;
-	// Use this for initialization
-	void Start () {
+	public void BuildTerrain() {
+		//if(GroundMaterial == null) {
+		//	Debug.Log("No material found, cannot render");
+		//	return;
+		//}
+
+		var before = DateTime.Now;
 		// create the world
-		ground = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		
+		var ground = transform;
 		var size = MediumSize;
 		if(Size == TerraformSize.Small) {
 			size = SmallSize;
@@ -31,13 +39,29 @@ public class Terraform : MonoBehaviour {
 		} else if(Size == TerraformSize.Large) {
 			size = LargeSize;
 		}
-
+		
 		ground.transform.localScale = size;
 		ground.transform.position = Position;
-		GroundMaterial.SetInt("Flow", 1000);
-		ground.renderer.material = GroundMaterial;
-		Debug.Log (ground.renderer.material.GetFloat("Flow"));
+		//GroundMaterial.SetInt("Flow", 1000);
+		//ground.renderer.sharedMaterial = GroundMaterial;
+
+		var subStance = ground.renderer.sharedMaterial as ProceduralMaterial;
+		var props = subStance.GetProceduralPropertyDescriptions();
+		//props.ToList().ForEach( o => Debug.Log("Name: " + o.name + ", value: " + o.step.ToString()));
+
+		subStance.SetProceduralFloat("Zoom", 1.0f);
+		subStance.RebuildTextures();
+
+		var after = DateTime.Now.Subtract(before).Milliseconds;
+		Debug.Log("Done in: " + after.ToString() + "ms");
+		//Debug.Log (ground.renderer.material.GetFloat("Flow"));
 		//ground.renderer.material.SetFloat("Zoom", 1.0f);
+	}
+	
+	GameObject pointLight;
+	// Use this for initialization
+	void Start () {
+		BuildTerrain();
 	}
 	
 	// Update is called once per frame
