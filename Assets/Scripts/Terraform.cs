6503fbcff8da2570cab5cad45fe21ff6bfb025d1
Roger Larsen <rlarsen@hmstn.com>
2014-04-03 15:25:20 -0500

    added water resources to terra form

--- Assets/Scripts/Terraform.cs
+++ Assets/Scripts/Terraform.cs
@@ -37,14 +37,36 @@ public class Terraform : MonoBehaviour {
 		
 		ground.transform.localScale = size;
 		ground.transform.position = Position;
-		var subStance = ground.renderer.sharedMaterial as ProceduralMaterial;
+		//var subStance = ground.renderer.sharedMaterial as ProceduralMaterial;
 		//var props = subStance.GetProceduralPropertyDescriptions();
 		//props.ToList().ForEach( o => Debug.Log("Name: " + o.name + ", value: " + o.step.ToString()));
 
 		//subStance.SetProceduralFloat("Zoom", 1.0f);
-		subStance.SetProceduralFloat("$randomseed", UnityEngine.Random.Range(0, 10000));
-		subStance.RebuildTextures();
+		//subStance.SetProceduralFloat("$randomseed", UnityEngine.Random.Range(0, 10000));
+		//subStance.RebuildTextures();
 
+		var noise = new SimplexNoiseGenerator();
+		// test creating a noise texture for fun
+		var tex = new Texture2D(256, 256);
+		for(var x = 0; x < tex.width; x++) {
+			for(var y = 0; y < tex.height; y++) {
+				// multiplier affects how large the patter is; the large the number the bigger the waves (noise)
+				// basically a small value will leave a bunch of small specles, large number larger objects
+				// noise.coherentNoise(x, y, 0, 10, 75, 1); -> looks like a star map
+				// noise.coherentNoise(x, y, 0, 1, 75, 2); -> clouds
+				var noi = noise.coherentNoise(x, y, 0, 1, 75, 2);
+				var color = new Color();
+				if(noi > 0.2f) {
+					color = new Color(0, 0, noi, noi);
+				} else {
+					color = new Color(0, 0, 0, 0);
+				}
+				tex.SetPixel(x, y, color);
+			}
+		}
+		tex.filterMode = FilterMode.Trilinear;
+		tex.Apply();
+		ground.renderer.sharedMaterial.mainTexture = tex;
 		//var after = DateTime.Now.Subtract(before).Milliseconds;
 	}
 	
