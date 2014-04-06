1476ae66c7faa829b25ff28d351bda356dea30c0
Roger Larsen <rlarsen@hmstn.com>
2014-04-06 01:44:53 -0500

    adding health bars

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Camera.main.transform);
+		//transform.Rotate(new Vector3(0, 45,0));
+	}
+}
